3a2acac2617d4e9873473b53da730c89771cd93e
Miguel Angel <miguelapb35@gmail.com>
2020-05-17 21:01:01 -0500

    Updates

---
+++ precache-manifest.f5b0e1904aadb1a7e085fde239ff8904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf36203d68246667bb0f32e1c5a9b02",
+    "url": "/firebase-react-tutorial/index.html"
+  },
+  {
+    "revision": "98a8030f7bccf2df5253",
+    "url": "/firebase-react-tutorial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "06dea6ae8df52c55d515",
+    "url": "/firebase-react-tutorial/static/js/2.b2e6b705.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/firebase-react-tutorial/static/js/2.b2e6b705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a8030f7bccf2df5253",
+    "url": "/firebase-react-tutorial/static/js/main.be845edf.chunk.js"
+  },
+  {
+    "revision": "157b9cb4602572006154",
+    "url": "/firebase-react-tutorial/static/js/runtime-main.34af1d79.js"
+  }
+]);
